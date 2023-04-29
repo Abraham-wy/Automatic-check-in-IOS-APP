48ed81ea0272651cde0833381780f7223afe1f5b
Abraham <91005467+Abraham-wy@users.noreply.github.com>
2023-04-29 14:28:14 +0800

    Update auto-checkin.js

--- .github/workflows/auto-checkin.js
+++ .github/workflows/auto-checkin.js
@@ -6,9 +6,9 @@ const puppeteer = require('puppeteer');
 
   // 登录操作
   await page.goto('https://www.iios.fun/login');
-  await page.type('input[name="username"]', '3501654994@qq.com');
+  await page.type('input[name="email"]', '3501654994@qq.com');
   await page.type('input[name="password"]', '18024532933wysbd');
-  await page.click('form button[type="submit"]');
+  await page.click('form button[type="提交"]');
   await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
   // 签到操作
