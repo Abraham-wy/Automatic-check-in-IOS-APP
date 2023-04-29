8c02b34dab94f8e21ad51ffbb9c8ee3558d9ebce
Abraham <91005467+Abraham-wy@users.noreply.github.com>
2023-04-29 14:43:37 +0800

    Update auto-checkin.js

--- .github/workflows/auto-checkin.js
+++ .github/workflows/auto-checkin.js
@@ -1,18 +1,19 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
 
-  // 登录操作
-  await page.goto('https://www.iios.fun/login');
-  await page.type('input[name="email"]', '3501654994@qq.com');
-  await page.type('input[name="password"]', '18024532933wysbd');
-  await page.click('button.N44eQI_9');
-  await page.waitForNavigation({ waitUntil: 'networkidle0' });
+    // 登录操作
+    await page.goto('https://www.iios.fun/login');
+    await page.type('input[name="email"]', '3501654994@qq.com');
+    await page.type('input[name="password"]', '18024532933wysbd');
+    await page.click('button.N44eQI_9');
+    await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
-  // 签到操作
-  await page.goto('https://www.iios.fun/points');
-  await page.click('.gm-checkin-panel .checkin.availabel:not(.checked)');
-  await browser.close();
+    // 签到操作
+    await page.goto('https://www.iios.fun/points');
+    await page.waitForSelector('.gm-checkin-panel .checkin.availabel:not(.checked)');
+    await page.click('.gm-checkin-panel .checkin.availabel:not(.checked)');
+    await browser.close();
 })();
